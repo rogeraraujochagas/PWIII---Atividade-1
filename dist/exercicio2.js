c426be998291eda83735d982e724372aa9f5a0de
Rogerio <rogeri_22@hotmail.com>
2019-08-23 20:16:09 -0300

    exercicio3

--- dist/exercicio2.js
+++ dist/exercicio2.js
@@ -1,2 +1,3 @@
 var salario = 1500.98;
 console.log(salario);
+console.log("Sal\u00E1rio informado \u00E9 de R$ " + salario);
--- exercicio2.ts
+++ exercicio2.ts
@@ -1,2 +1,4 @@
 let salario: number = 1500.98
 console.log(salario)
+
+console.log(`Salário informado é de R$ ${salario}`)
