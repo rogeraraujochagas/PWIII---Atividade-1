9130955dab6dfc985a0906d27773cbbd403dbe4d
Rogerio <rogeri_22@hotmail.com>
2019-08-23 20:34:49 -0300

    exercicio4

---
+++ dist/exercicio4-5.js
@@ -0,0 +1,10 @@
+var n = 5;
+var funcao = function (nm) {
+    if (n % 2 == 0) {
+        console.log('Par');
+    }
+    else {
+        console.log('Ímpar');
+    }
+};
+funcao(n);
---
+++ exercicio4-5.ts
@@ -0,0 +1,12 @@
+let n: number = 5
+
+let funcao = function (nm: number): void{
+    if(n%2 == 0){
+        console.log('Par')
+    }
+    else{
+        console.log('Ímpar')
+    }
+}
+
+funcao(n)
