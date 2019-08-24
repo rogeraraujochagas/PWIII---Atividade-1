3f467b41104436fddeb0e7b7342d3a5fd5ee56c1
Rogerio <rogeri_22@hotmail.com>
2019-08-23 22:17:34 -0300

    exercicio10

---
+++ dist/exercicio10.js
@@ -0,0 +1,18 @@
+"use strict";
+exports.__esModule = true;
+exports.funcao = function (nm) {
+    if (n % 2 == 0) {
+        //console.log('Par')
+        return true;
+    }
+    else {
+        //console.log('Ímpar')
+        return false;
+    }
+};
+exports.area = function (lar, comp) {
+    return lar * comp;
+};
+exports.cubo = function (c) {
+    return c * c * c;
+};
---
+++ exercicio10.ts
@@ -0,0 +1,19 @@
+export let funcao = function (nm: number):/*void*/ boolean{
+    if(n%2 == 0){
+        //console.log('Par')
+        return true
+    }
+    else{
+        //console.log('Ímpar')
+        return false
+    }
+}
+
+export let area = function (lar : number, comp : number){
+    return lar * comp
+}
+
+export let cubo = (c: number) => {
+    return c * c * c   
+ }
+ 
