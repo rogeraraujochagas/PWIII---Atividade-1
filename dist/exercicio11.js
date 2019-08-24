e6a44929a0c3185b48bec15c507b213eff615bc1
Rogerio <rogeri_22@hotmail.com>
2019-08-23 22:19:55 -0300

    exercicio11

---
+++ dist/exercicio11.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var exercicio10_1 = require("./exercicio10");
+console.log(exercicio10_1.cubo(6));
---
+++ exercicio11.ts
@@ -0,0 +1,3 @@
+import {cubo} from "./exercicio10"
+
+console.log(cubo(6))
