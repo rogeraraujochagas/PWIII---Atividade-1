e5d82b31a1e8376e67ee622e38003d533e18ce8e
Rogerio <rogeri_22@hotmail.com>
2019-08-23 21:34:06 -0300

    exercicio6

---
+++ dist/exercicio6.js
@@ -0,0 +1,6 @@
+var largura = 5;
+var comprimento = 10;
+var area = function (lar, comp) {
+    return lar * comp;
+};
+console.log(area(largura, comprimento));
---
+++ exercicio6.ts
@@ -0,0 +1,8 @@
+let largura : number = 5
+let comprimento : number = 10
+
+let area = function (lar : number, comp : number){
+    return lar * comp
+}
+
+console.log(area(largura, comprimento))
